defcfad38702ac8f84a6ad0429c4e2b737f52e9d
Augustin Mauroy <augustin.mauroy@outlook.fr>
2025-01-17 17:52:19 +0100

    simplify next.js

--- next.config.ts
+++ next.config.ts
@@ -5,18 +5,13 @@ const withNextIntl = createNextIntlPlugin('./lib/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   // need to be test on CI but not on building of the app
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  typescript: { ignoreBuildErrors: true },
   // We didn't use eslint
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   // Static website output
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: false,
-  transpilePackages: ['geist', 'next-mdx-remote'],
 };
 
 export default withNextIntl(nextConfig);
